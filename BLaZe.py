b0fca49100b8f969bc166bcc9f17586f0a0f8db1
TrAm aLeX <87557914+TEAM-BLAZ@users.noreply.github.com>
2021-08-10 14:45:25 +0530

    Update BLaZe.py

--- BLaZe.py
+++ BLaZe.py
@@ -6,7 +6,7 @@ from os import execl
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
-from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10 
+from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2 
 import asyncio
 import telethon.utils
 from telethon.tl import functions
@@ -18,25 +18,9 @@ api = API_ID
 hash = API_HASH
 blazea = STRING
 blazeb = STRING2
-blazec = STRING3
-blazed = STRING4
-blazee = STRING5
-blazef = STRING6
-blazeg = STRING7
-blazeh = STRING8
-blazei = STRING9
-blazej = STRING10
 
 bla = ""
 blb = ""
-blc = ""
-bld = ""
-ble = ""
-blf = ""
-blg = ""
-blh = ""
-bli = ""
-blj = ""
 
 que = {}
 
@@ -47,14 +31,7 @@ for x in SUDO:
 async def start_BLaZe():
     global bla
     global blb
-    global blc
-    global bld
-    global ble
-    global blf
-    global blg
-    global blh
-    global bli
-    global blj
+
 
     if blazea:
         session_name = str(blazea)
@@ -78,5 +55,30 @@ async def start_BLaZe():
         bla = TelegramClient(session_name, api, hash)
         try:
             await bla.start()
+        except Exception as e:
+            pass
+
+    if blazeb:
+        session_name = str(blazeb)
+        print("String 1 Found")
+        blb = TelegramClient(StringSession(session_name), api, hash)
+        try:
+            print("Booting Up The Client 1")
+            await blb.start()
+            botme = await blb.get_me()
+            await blb(functions.channels.JoinChannelRequest(channel="@BLAZE_SPAMMER"))
+            await blb(functions.channels.JoinChannelRequest(channel="@BLAZE_ZONE"))           
+            botid = telethon.utils.get_peer_id(botme)
+            BLAZEA_USERS.append(botid)
+        except Exception as e:
+          
+            print(e)
+            pass
+    else:
+        print("Session 1 not Found")
+        session_name = "startup"
+        blb = TelegramClient(session_name, api, hash)
+        try:
+            await blb.start()
         except Exception as e:
             pass
