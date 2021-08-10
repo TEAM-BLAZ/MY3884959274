486a44f43f143d5e9cd659aaae75568cd8cdb4ce
TrAm aLeX <87557914+TEAM-BLAZ@users.noreply.github.com>
2021-08-10 14:27:38 +0530

    Create BLaZe.py

---
+++ BLaZe.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import random
+from datetime import datetime
+from os import execl
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateProfileRequest
+from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10 
+import asyncio
+import telethon.utils
+from telethon.tl import functions
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from Utils import RAID, RRAID
+
+api = API_ID
+hash = API_HASH
+blazea = STRING
+blazeb = STRING2
+blazec = STRING3
+blazed = STRING4
+blazee = STRING5
+blazef = STRING6
+blazeg = STRING7
+blazeh = STRING8
+blazei = STRING9
+blazej = STRING10
